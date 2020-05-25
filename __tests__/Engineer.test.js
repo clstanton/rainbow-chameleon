1c004cf8f0f7dbe22e489642adef10ca994cf969
clstanton <cstanton77@gmail.com>
2020-05-24 21:40:54 -0500

    engineer test passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,10 +1,10 @@
 const Engineer = require('../lib/Engineer.js');
 
-test('creates an engineer object',() => {
-    const engineer = new Engineer('Ryan McDonald', '12345', 'rmcdonald@tbdemail.com', 'rmcdonald');
+test('creates an engineer object', () => {
+    const engineer = new Engineer('Ryan McDonald', '12345', 'rmcdonald@tbdemail.com', 'rsmcdonald');
 
     expect(engineer.name).toBe('Ryan McDonald');
     expect(engineer.id).toBe('12345');
     expect(engineer.email).toBe('rmcdonald@tbdemail.com');
-    expect(engineer.github).toBe('rmcdonald');
+    expect(engineer.github).toBe('rsmcdonald');
 });
